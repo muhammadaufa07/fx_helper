a3f0e5235ba4d3a269e04d49449dbf71f4941e68
MuhammadAufa07 <62533996+muhammadaufa07@users.noreply.github.com>
2025-09-09 13:52:48 +0700

    change regexp helper to static function

--- lib/regex_helper.dart
+++ lib/regex_helper.dart
@@ -1,5 +1,5 @@
 class RegexpHelper {
-  bool isEmail(String email) {
+  static bool isEmail(String email) {
     // String regex =
     // r"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$";
     String regex = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
@@ -7,19 +7,19 @@ class RegexpHelper {
     return regExp.hasMatch(email);
   }
 
-  bool isPhoneNumber(String phoneNumber) {
+  static bool isPhoneNumber(String phoneNumber) {
     String regex = r'^8[\d]{8,15}$';
     RegExp regExp = RegExp(regex);
     return regExp.hasMatch(phoneNumber);
   }
 
-  bool isNPWP(String npwp) {
+  static bool isNPWP(String npwp) {
     String regex = r'^\d{2}\.\d{3}\.\d{3}\.\d{1}-\d{3}\.\d{3}$';
     RegExp regExp = RegExp(regex);
     return regExp.hasMatch(npwp);
   }
 
-  bool isPassword(String password) {
+  static bool isPassword(String password) {
     RegExp hasUppercase = RegExp(r'(?=.*[A-Z])');
     RegExp hasLowercase = RegExp(r'(?=.*[a-z])');
     RegExp hasDigit = RegExp(r'(?=.*[0-9])');
@@ -33,7 +33,7 @@ class RegexpHelper {
         hasMinLength.hasMatch(password);
   }
 
-  String getPasswordWarning(String password) {
+  static String getPasswordWarning(String password) {
     String warning = '';
 
     if (!RegExp(r'(?=.*[A-Z])').hasMatch(password)) {
