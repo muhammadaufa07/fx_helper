8c1d100b2a998784dea7a4f7200260b3ebb15392
MuhammadAufa07 <62533996+muhammadaufa07@users.noreply.github.com>
2025-11-11 11:12:58 +0700

    add in app notification

---
+++ lib/in_app_notification.dart
@@ -0,0 +1,251 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class InAppNotification {
+  static final List<OverlayEntry> _e = [];
+  static final double _stackOffset = 2;
+
+  static void show(
+    BuildContext context, {
+    required String title,
+    required String message,
+    Widget? icon,
+    required Color color,
+    Duration? duration,
+    Alignment? alignment,
+  }) {
+    late OverlayEntry overlayEntry;
+    overlayEntry = OverlayEntry(
+      builder: (context) {
+        int multiplier = _e.indexOf(overlayEntry);
+        return Positioned(
+          bottom: MediaQuery.of(context).padding.top + 30 + ((multiplier) * _stackOffset),
+          left: 16 + ((multiplier) * 1),
+          right: 16 - ((multiplier) * 1),
+          child: InAppNotifCard(
+            title: title,
+            message: message,
+            icon: icon,
+            color: color,
+            duration: duration,
+            onClose: () {
+              if (overlayEntry.mounted) overlayEntry.remove();
+              _e.remove(overlayEntry);
+            },
+          ),
+        );
+      },
+    );
+    _e.add(overlayEntry);
+
+    final overlay = Overlay.of(context);
+
+    overlay.insert(overlayEntry);
+    // kalau ada durasi, auto dismiss
+    if (duration != null) {
+      Future.delayed(duration, () {
+        if (overlayEntry.mounted) overlayEntry.remove();
+        _e.remove(overlayEntry);
+      });
+    }
+  }
+}
+
+class InAppNotifCard extends StatefulWidget {
+  final String title;
+  final String message;
+  final Widget? icon;
+  final Color color;
+  final VoidCallback onClose;
+  final Duration? duration;
+
+  const InAppNotifCard({
+    super.key,
+    required this.title,
+    required this.message,
+    this.icon,
+    required this.color,
+    required this.onClose,
+    this.duration,
+  });
+
+  @override
+  State<InAppNotifCard> createState() => _InAppNotifCardState();
+}
+
+class _InAppNotifCardState extends State<InAppNotifCard> with TickerProviderStateMixin {
+  final GlobalKey _containerKey = GlobalKey();
+  late AnimationController _slideController;
+  Animation<Offset>? _slideAnimation;
+  AnimationController? _progressController;
+  double _backdropHeight = 0;
+
+  void _getBackdropHeight() {
+    if (_containerKey.currentWidget != null) {
+      final RenderBox? renderBox = _containerKey.currentContext?.findRenderObject() as RenderBox?;
+      if (renderBox != null) {
+        _backdropHeight = renderBox.size.height;
+      }
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // Slide in animasi
+    _slideController = AnimationController(vsync: this, duration: const Duration(milliseconds: 1000));
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, -1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _slideController, curve: Curves.easeOutCubic));
+
+    _slideController.forward();
+
+    // Kalau ada durasi, bikin progress bar countdown
+    if (widget.duration != null) {
+      _progressController = AnimationController(vsync: this, duration: widget.duration)..forward();
+    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _getBackdropHeight();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _slideController.dispose();
+    _progressController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color pColor = widget.color;
+    return Material(
+      color: Colors.transparent,
+      child: Row(
+        children: [
+          Expanded(
+            child: SlideTransition(
+              position: _slideAnimation!,
+              child: Dismissible(
+                key: UniqueKey(),
+                direction: DismissDirection.vertical, // bisa swipe ke atas buat close
+                onDismissed: (_) => widget.onClose(),
+
+                child: Stack(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadiusGeometry.circular(12),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+                        child: Builder(
+                          builder: (context) {
+                            return Container(height: _backdropHeight);
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      key: _containerKey,
+                      padding: EdgeInsets.all(14),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withValues(alpha: 0.9),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: pColor, width: 1),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              widget.icon ?? Icon(Icons.check_circle, color: pColor, size: 52),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      widget.title,
+                                      style: TextStyle(fontWeight: FontWeight.w800, fontSize: 14, color: pColor),
+                                    ),
+                                    const SizedBox(height: 2),
+                                    Text(
+                                      widget.message,
+                                      style: GoogleFonts.inter(
+                                        fontWeight: FontWeight.w600,
+                                        color: pColor,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              IconButton(
+                                visualDensity: VisualDensity.compact,
+                                icon: Icon(Icons.close, color: pColor),
+                                onPressed: widget.onClose,
+                              ),
+                            ],
+                          ),
+                          if (_progressController != null) ...[
+                            const SizedBox(height: 4),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Container(
+                                padding: const EdgeInsets.all(1),
+                                decoration: BoxDecoration(color: pColor, borderRadius: BorderRadius.circular(20)),
+                                child: AnimatedBuilder(
+                                  animation: _progressController!,
+                                  // builder: (context, child) => LinearProgressIndicator(
+                                  //   value: 1.0 - (_progressController?.value ?? 0),
+                                  //   backgroundColor: Colors.white,
+                                  //   borderRadius: BorderRadius.circular(20),
+                                  //   valueColor: AlwaysStoppedAnimation<Color>(
+                                  //     pColor.withValues(alpha: 1 - ((_progressController?.value ?? 0) * 0.5)),
+                                  //   ),
+                                  //   minHeight: 16,
+                                  // ),
+                                  builder: (context, child) => Container(
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      gradient: LinearGradient(
+                                        colors: [
+                                          pColor.withValues(
+                                            red: (pColor.r * 1.2),
+                                            green: (pColor.g * 1.2),
+                                            blue: (pColor.b * 1.2),
+                                          ),
+                                          pColor,
+                                          Colors.white,
+                                        ],
+                                        stops: [
+                                          (1 - (_progressController?.value ?? 0)) / 2,
+                                          (1 - (_progressController?.value ?? 0)),
+                                          (1 - (_progressController?.value ?? 0)),
+                                        ],
+                                      ),
+                                    ),
+                                    child: const SizedBox(height: 6),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/snackbar_helper.dart
+++ lib/snackbar_helper.dart
@@ -1,4 +1,6 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
+import 'package:fx_helper/in_app_notification.dart';
 
 /// A global key for showing SnackBars across the entire app
 /// using [SnackbarHelper.showSnackBar].
@@ -11,6 +13,7 @@ import 'package:flutter/material.dart';
 /// )
 /// ```
 final GlobalKey<ScaffoldMessengerState> globalSnackbarKey = GlobalKey<ScaffoldMessengerState>();
+final GlobalKey<NavigatorState> globalNavigatorKey = GlobalKey<NavigatorState>();
 
 /// Different types of snackbar states that define color and style.
 enum SnackbarState { success, error, warning }
@@ -25,40 +28,6 @@ enum SnackbarState { success, error, warning }
 /// );
 /// ```
 class SnackbarHelper {
-  /// Returns the text color for the snackbar based on its [SnackbarState].
-  ///
-  /// Example:
-  /// ```dart
-  /// Color? textColor = SnackbarHelper._getTextColor(SnackbarState.error); // Colors.white
-  /// ```
-  static Color? _getTextColor(SnackbarState state) {
-    switch (state) {
-      case SnackbarState.success:
-        return Colors.white;
-      case SnackbarState.error:
-        return Colors.white;
-      case SnackbarState.warning:
-        return Colors.white;
-    }
-  }
-
-  /// Returns the background color for the snackbar based on its [SnackbarState].
-  ///
-  /// Example:
-  /// ```dart
-  /// Color? bg = SnackbarHelper._getbackground(SnackbarState.error); // Colors.red
-  /// ```
-  static Color? _getbackground(SnackbarState state) {
-    switch (state) {
-      case SnackbarState.success:
-        return null;
-      case SnackbarState.error:
-        return Colors.red;
-      case SnackbarState.warning:
-        return Colors.orange;
-    }
-  }
-
   /// Shows a Snackbar with custom styles depending on [SnackbarState].
   ///
   /// Parameters:
@@ -116,5 +85,111 @@ class SnackbarHelper {
       ),
     );
     globalSnackbarKey.currentState?.showSnackBar(snackBar);
+  }
+
+  /// Avoid when message more than 2 lines
+  /// Usage:
+  ///   SnackbarHelper.showSnackBar2(
+  ///     SnackbarState.success,
+  ///     "Message",
+  ///   );
+  static void fancy(
+    SnackbarState state,
+    String message, {
+    bool showButton = false,
+    String? buttonTitle,
+    Function()? onTap,
+    Duration? duration,
+  }) {
+    final snackBar = SnackBar(
+      /// need to set following properties for best effect of awesome_snackbar_content
+      elevation: 0,
+      behavior: SnackBarBehavior.floating,
+      duration: duration ?? const Duration(seconds: 2),
+      backgroundColor: Colors.transparent,
+      content: AwesomeSnackbarContent(
+        /*  */
+        messageTextStyle: TextStyle(overflow: TextOverflow.visible),
+        inMaterialBanner: true,
+        title: _getTitle(state),
+        message: message,
+        contentType: _getContentType(state),
+      ),
+    );
+    globalSnackbarKey.currentState?.showSnackBar(snackBar);
+  }
+
+  static void inApp(
+    BuildContext context,
+    SnackbarState state,
+    String message, {
+    bool showButton = false,
+    String? buttonTitle,
+    Function()? onTap,
+    Duration? duration,
+  }) {
+    InAppNotification.show(
+      context,
+      icon: _getIcon(state, _getbackground(state) ?? Colors.green),
+      title: _getTitle(state),
+      message: message,
+      color: _getbackground(state) ?? Colors.green,
+      duration: duration ?? Duration(seconds: 5),
+    );
+  }
+
+  static Widget _getIcon(SnackbarState state, Color color) {
+    switch (state) {
+      case SnackbarState.success:
+        return Icon(Icons.check_circle, color: color, size: 48);
+      case SnackbarState.error:
+        return Icon(Icons.close, color: color, size: 48);
+      case SnackbarState.warning:
+        return Icon(Icons.donut_large_rounded, color: color, size: 48);
+    }
+  }
+
+  static Color? _getTextColor(SnackbarState state) {
+    switch (state) {
+      case SnackbarState.success:
+        return Colors.white;
+      case SnackbarState.error:
+        return Colors.white;
+      case SnackbarState.warning:
+        return Colors.white;
+    }
+  }
+
+  static Color? _getbackground(SnackbarState state) {
+    switch (state) {
+      case SnackbarState.success:
+        return null;
+      case SnackbarState.error:
+        return Colors.red;
+      case SnackbarState.warning:
+        return Colors.orange;
+    }
+  }
+
+  static ContentType _getContentType(SnackbarState state) {
+    switch (state) {
+      case SnackbarState.success:
+        return ContentType.success;
+      case SnackbarState.error:
+        return ContentType.failure;
+      case SnackbarState.warning:
+        return ContentType.warning;
+    }
+  }
+
+  static String _getTitle(SnackbarState state) {
+    switch (state) {
+      case SnackbarState.success:
+        return "Success";
+      case SnackbarState.error:
+        return "Gagal";
+      case SnackbarState.warning:
+        return "Notice";
+    }
   }
 }
