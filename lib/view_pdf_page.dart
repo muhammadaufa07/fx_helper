174d040ab1d6cd7d38b2c0bd9706bedaf8129826
MuhammadAufa07 <62533996+muhammadaufa07@users.noreply.github.com>
2025-11-19 09:10:50 +0700

    change pdf page

--- lib/view_pdf_page.dart
+++ lib/view_pdf_page.dart
@@ -94,6 +94,7 @@ class _ViewPdfPageState extends State<ViewPdfPage> {
 
   @override
   Widget build(BuildContext context) {
+    Color primaryColor = Theme.of(context).primaryColor;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar:
@@ -143,7 +144,7 @@ class _ViewPdfPageState extends State<ViewPdfPage> {
           ? IconButton(
               icon: Container(
                 padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: const Color(0xFFAC1F1F)),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: primaryColor),
                 child: Icon(Icons.download, color: Colors.white),
               ),
               onPressed: () {
@@ -154,7 +155,7 @@ class _ViewPdfPageState extends State<ViewPdfPage> {
       body: Builder(
         builder: (context) {
           if (isLoading) {
-            return Center(child: CircularProgressIndicator(color: const Color(0xFFAC1F1F)));
+            return Center(child: CircularProgressIndicator(color: primaryColor));
           }
 
           if (widget.path == null || widget.path?.isEmpty == true) {
@@ -164,7 +165,7 @@ class _ViewPdfPageState extends State<ViewPdfPage> {
           }
 
           return SfPdfViewerTheme(
-            data: SfPdfViewerThemeData(backgroundColor: Colors.white, progressBarColor: const Color(0xFFAC1F1F)),
+            data: SfPdfViewerThemeData(backgroundColor: Colors.white, progressBarColor: primaryColor),
             child: Builder(
               builder: (context) {
                 if (widget.viewPdfType == ViewPdfType.asset) {
