3b4d12f5f9431a9598fd5b77d320ea4a59d72bfa
irfan fadhila <irfanfadhihla000@gmail.com>
2026-07-20 09:19:03 +0700

    feat: ontap on net msg dialog

--- lib/widgets/net_msg_dialog.dart
+++ lib/widgets/net_msg_dialog.dart
@@ -11,7 +11,7 @@ class NetMsgDialog {
   static bool widgetInView = false;
   static final GlobalKey _networkDialogGlobalKey = GlobalKey();
 
-  static void handleError(BuildContext context, e, http.Response? res, Function()? onTap) async {
+  static void handleError(BuildContext context, e, http.Response? res, {Function()? onTap}) async {
     // print(e);
     // print(res);
     if (widgetInView || _networkDialogGlobalKey.currentWidget != null) return;
